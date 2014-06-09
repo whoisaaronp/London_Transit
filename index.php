a9d2d7d7a9b4a21c75c4ebec85c1e735a249f028
spiderPan <panbanglanfeng@gmail.com>
2014-06-09 17:16:00 -0400

    add gitignore

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pan
+ * Date: 6/9/2014
+ * Time: 4:41 PM
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="UTF-8">
+<title>London Transit</title>
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script src="scripts/scripts.js"></script>
+<style>
+	#map_canvas {
+		width: 500px;
+		height: 400px;
+	}
+</style>
+</head>
+<body>
+<div id="map_canvas"></div>
+</body>
+</html>
