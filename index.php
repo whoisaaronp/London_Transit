ba2ab442ec4f8641aed0ef153c9990950e473e98
spiderPan <panbanglanfeng@gmail.com>
2014-06-16 16:54:48 -0400

    get location data

--- index.php
+++ index.php
@@ -5,6 +5,38 @@
  * Date: 6/9/2014
  * Time: 4:41 PM
  */
+
+/*#$stations        = json_decode(file_get_contents('media/stations.json'));
+$stations_coords = json_decode(file_get_contents('media/stations_coords.json'));
+
+function lookup($str) {
+	$str = str_replace(' ', '+', urlencode(trim($str)));
+
+	$target_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" . $str . "+London+ON&sensor=false";
+
+	$result = json_decode(file_get_contents($target_url));
+	if ('OK' === $result->status && $result->results > 0) {
+		$lat        = $result->results[0]->geometry->location->lat;
+		$lng        = $result->results[0]->geometry->location->lng;
+		$coordinate = array($lat, $lng);
+	} else {
+		$coordinate = 'null';
+	}
+
+	return $coordinate;
+}
+
+foreach ($stations_coords as $station) {
+
+	if ('null' == $station->coord) {
+		echo 'go';
+		$station->coord = lookup($station->Name);
+	}
+}
+
+echo file_put_contents('media/stations_coords.json', json_encode($stations_coords));*/
+
+
 ?>
 <!DOCTYPE html>
 <html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,9 @@
 /**
  * Created by pan on 6/9/2014.
  */
+
+//Take Number 7 of Route 17 as example
+
 function initialize() {
 	var mapOptions = {
 		center : new google.maps.LatLng(42.9929761, -81.2514245, 14),
@@ -9,5 +12,6 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 		mapOptions);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
