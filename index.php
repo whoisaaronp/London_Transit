283a0c8c8c6f02bda1bfdb2ef8579ed79fabfad2
spiderPan <panbanglanfeng@gmail.com>
2014-06-16 15:40:11 -0400

    init all scripts

--- index.php
+++ index.php
@@ -11,16 +11,17 @@
 <head lang="en">
 <meta charset="UTF-8">
 <title>London Transit</title>
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-<script src="scripts/scripts.js"></script>
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=visualization"></script>
+<script type="text/javascript" src="scripts/scripts.js"></script>
 <style>
 	#map_canvas {
-		width: 500px;
-		height: 400px;
+		width: 100%;
+		height: 600px;
+		margin: 0 auto;
 	}
 </style>
 </head>
-<body>
+<body onload="initialize()">
 <div id="map_canvas"></div>
 </body>
 </html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
 /**
  * Created by pan on 6/9/2014.
  */
+function initialize() {
+	var mapOptions = {
+		center : new google.maps.LatLng(42.9929761, -81.2514245, 14),
+		zoom   : 13
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),
+		mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
