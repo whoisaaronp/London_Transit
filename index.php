e0d6a45731da9ebc66e5bdf0696109683e690167
spiderPan <panbanglanfeng@gmail.com>
2014-06-18 17:59:03 -0400

    add 17 and 21

--- index.php
+++ index.php
@@ -1,73 +1,139 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: pan
  * Date: 6/9/2014
  * Time: 4:41 PM
  */
+class Load_Station_Data {
+	private $_stations;
+	private $_path = 'media/stations/';
+	private $_url = null;
 
-
-/*$stations = json_decode(file_get_contents('media/stations/21_stations.json'));
-
-function lookup($str) {
-	$str_filter = array(
-		' ' => '+',
-		'&' => 'at'
-	);
-	$str        = strtr(urlencode(trim($str)), $str_filter);
-
-	$target_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" . $str . "+London+ON+CA&sensor=false";
-
-	$result = json_decode(file_get_contents($target_url));
-	if ('OK' === $result->status && $result->results > 0) {
-		$lat        = $result->results[0]->geometry->location->lat;
-		$lng        = $result->results[0]->geometry->location->lng;
-		$coordinate = array($lat, $lng);
-	} else {
-		$coordinate = null;
+	private function get_station() {
+		try {
+			$this->_stations = json_decode(file_get_contents($this->_url));
+		} catch (Exception $e) {
+			echo 'Caught exception: ', $e->getMessage(), "\n";
+		}
 	}
 
-	return $coordinate;
+	private function set_station() {
+		try {
+			file_put_contents($this->_url, json_encode($this->_stations));
+		} catch (Exception $e) {
+			echo 'Caught exception: ', $e->getMessage(), "\n";
+		}
+	}
+
+	private function lookup($str) {
+		$str_filter = array(
+			' '    => '+',
+			'&'    => 'at',
+			'of'   => 'at',
+			'Stop' => '',
+			'WB'   => '',
+			'EB'   => '',
+			'Leg'  => ''
+		);
+		$str        = strtr(urlencode(trim($str)), $str_filter);
+
+		$target_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" . $str . "+London+ON+CA&sensor=false";
+
+		$result = json_decode(file_get_contents($target_url));
+		if ('OK' === $result->status && $result->results > 0) {
+			$lat = $result->results[0]->geometry->location->lat;
+			$lng = $result->results[0]->geometry->location->lng;
+			if (!in_array('political', $result->results[0]->types)) {
+				$coordinate = array($lat, $lng);
+			} else {
+				$coordinate = null;
+			}
+		} else {
+			$coordinate = null;
+		}
+
+		return $coordinate;
+	}
+
+	public function load($number) {
+		$this->_url = $this->_path . $number . '_stations.json';
+		$this->get_station();
+		$station_cache = $this->_stations;
+		foreach ($this->_stations as $station) {
+
+			if (!isset($station->Coords) || null == $station->Coords) {
+				echo 'go' . PHP_EOL;
+				$coords          = (null != $this->lookup($station->Announcement)) ? $this->lookup($station->Announcement) : $this->lookup($station->Name);
+				$station->Coords = $coords;
+			}
+
+			if (isset($station->Name)) {
+				$station->Name = trim($station->Name);
+			}
+
+			if (isset($station->Announcement)) {
+				$station->Announcement = trim($station->Announcement);
+			}
+		}
+		if ($station_cache !== $this->_stations) {
+			$this->set_station();
+		}
+	}
 }
 
 
-foreach ($stations as $station) {
-
-	if (!isset($station->Coords) || 1 != $station->Coords) {
-		echo 'go' . PHP_EOL;
-		$coords          = lookup($station->Name) ? lookup($station->Name) : lookup($station->Announcement);
-		$station->Coords = $coords;
-	}
-
-	if (isset($station->Name)) {
-		$station->Name = trim($station->Name);
-	}
-
-	if (isset($station->Announcement)) {
-		$station->Announcement = trim($station->Announcement);
-	}
-}
-
-echo file_put_contents('media/stations/21_stations.json', json_encode($stations));*/
+$station = new Load_Station_Data();
+$station->load('17');
 
 ?>
 <!DOCTYPE html>
-<html>
+<html ng-app>
 <head lang="en">
 <meta charset="UTF-8">
 <title>London Transit</title>
+
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=visualization"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.13/angular.min.js"></script>
+
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+
 <script type="text/javascript" src="scripts/scripts.js"></script>
 <style>
 	#map_canvas {
 		width: 100%;
-		height: 600px;
+		height: 700px;
 		margin: 0 auto;
 	}
 </style>
 </head>
 <body>
-<div id="map_canvas"></div>
+<div class="container-fluid">
+	<div class="row">
+		<div id="map_canvas"></div>
+	</div>
+	<div class="row" ng-controller="switchBus">
+		<form role="form">
+			<div class="form-group">
+				<label for="busNumber">Bus Number:{{busNumber}}</label>
+				<select id="busNumber" ng-model="busNumber" ng-change="busChange()" class="form-control input-lg">
+					<option ng-repeat="bus in busInfo | filter:bus.number">{{bus.number}}</option>
+				</select>
+			</div>
+		</form>
+	</div>
+</div>
+
 </body>
 </html>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,55 @@
 /**
  * Created by pan on 6/9/2014.
  */
-jQuery(document).ready(function ($) {
-	var markers = [];
 
-	var _url = '/media/stations/21_stations.json';
-	$.get(_url, function (data) {
-		markers = data;
-		initialize(markers);
-	});
-});
+function getByAttr(arr, attr, value) {
+	for (var d = 0, len = arr.length; d < len; d += 1) {
+		if (arr[d][attr] === value) {
+			return arr[d];
+		}
+	}
+}
+
+function switchBus($scope, $http) {
+	var markers = [];
+	var interval = null;
+	$scope.busNumber = 17;
+	$scope.busInfo = [
+		{
+			'number' : 17,
+			'paths'  : [318, 275, 276, 1632, 1342, 1353, 500, 498, 493, 494, 729, 731, 732, 784]
+		},
+		{
+			'number' : 21,
+			'paths'  : [1470, 1501, 1466, 1463, 1495, 1464, 344, 1155, 1157, 2241, 1016, 232, 230, 231, 1663, 1245, 1244, 393, 392, 1620, 1016]
+		}
+	];
+
+	$scope.drawMap = function () {
+		var busObj = getByAttr($scope.busInfo, 'number', $scope.busNumber);
+		var _pathsID = busObj.paths;
+		$http.get('/media/stations/' + $scope.busNumber + '_stations.json').success(function (data) {
+			markers = data;
+			initialize(markers, _pathsID);
+		});
+	};
+	$scope.drawMap();
+	$scope.busChange = function () {
+		$scope.busNumber = parseInt(this.busNumber);
+		$scope.drawMap();
+	}
+
+}
 
 //Take Number 7 of Route 21 as example
 
-function initialize(markers) {
-	var _paths = [];
+function initialize(markers, _pathsID) {
+	var _paths = [], _markers = [];
+	if (typeof(interval) != 'undefined' && interval) {
+		window.clearInterval(interval);
+	}
+	//var _pathsID = ;
+
 	var _map = new google.maps.Map(document.getElementById("map_canvas"),
 		{
 			center : new google.maps.LatLng(42.9929761, -81.2514245, 14),
@@ -26,22 +61,24 @@ function initialize(markers) {
 				{
 					position : new google.maps.LatLng(markers[i].Coords[0], markers[i].Coords[1]),
 					map      : _map,
-					title    : markers[i].Announcement
+					title    : markers[i].ID + ' ' + markers[i].Announcement
 				}
 			);
+			var _path = {"lat" : markers[i].Coords[0], "lng" : markers[i].Coords[1]};
 
-			var _path = [markers[i].Coords[0], markers[i].Coords[1]];
-			if (!containsArray(_path, _paths)) {
-				_paths.push(_path);
+			//if (!containsObj(_path, _paths)) {
+			var index = _pathsID.indexOf(markers[i].ID);
+			if (index > -1) {
+				_paths[index] = _path;
+
+				_markers.push(mapMarkers);
 			}
-
-
-
 		}
-
 	}
+	console.log("Path:" + _paths);
 	_paths = converToPath(_paths);
-	console.log(_paths);
+
+	console.log("Markders:" + _markers);
 	var lineSymbol = {
 		path        : google.maps.SymbolPath.CIRCLE,
 		scale       : 8,
@@ -64,14 +101,14 @@ function initialize(markers) {
 function converToPath(_paths) {
 	var _newPaths = [];
 	for (var i = 0; i < _paths.length; i++) {
-		_newPaths.push(new google.maps.LatLng(_paths[i][0], _paths[i][1]));
+		_newPaths.push(new google.maps.LatLng(_paths[i].lat, _paths[i].lng));
 	}
 	return _newPaths;
 }
 
 function animateCircle() {
 	var count = 0;
-	window.setInterval(function () {
+	interval = window.setInterval(function () {
 		count = (count + 1) % 200;
 
 		var icons = line.get('icons');
@@ -80,10 +117,10 @@ function animateCircle() {
 	}, 200);
 }
 
-function containsArray(array, list) {
+function containsObj(obj, list) {
 	var i;
 	for (i = 0; i < list.length; i++) {
-		if (arraysEqual(list[i], array)) {
+		if (JSON.stringify(list[i]) === JSON.stringify(obj)) {
 			return true;
 		}
 	}
