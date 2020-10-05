4e7579631613f5cb04ce3ba4392176c58a949391
Pan <panb@tbkcreative.com>
2020-10-05 12:38:41 -0400

    added cleaned data

--- data/clean_bus.py
+++ data/clean_bus.py
@@ -1,32 +1,55 @@
 # %%
 import os
 try:
-    os.chdir(os.path.join(os.getcwd(), 'kepler/london_transit'))
+    os.chdir(os.path.join(os.getcwd(), 'data'))
     print(os.getcwd())
 except:
     pass
 
 # %% Import all packages
-import json  
+import json
 import pandas as pd
 from pandas.io.json import json_normalize
 
 # %% Import Data
-stops = pd.read_csv('./data/September-2018.csv')
-stops.head()
+stops = pd.read_csv('./raw/stops.csv')
+trips = pd.read_csv('./raw/trips.csv')
+stop_times = pd.read_csv('./raw/stop_times.csv')
+
+# %% Merge stops into full_trips
+full_trips = stop_times.merge(
+    stops[['stop_lat', 'stop_lon', 'stop_id']], on='stop_id')
+full_trips.head()
 
 
-# %% Count how many routes
-stops['Route Count'] = stops['Routes'].str.count(',') +1
-stops.head()
+# %% Merge Route Info into full_trips
+full_trips = full_trips.merge(trips[['trip_id', 'route_id']], on='trip_id')
+full_trips.head()
+
+# %% Define customized function
+
+
+def validate_time(date_str):
+    x = int(date_str.split(':', 1)[0])
+    if x >= 24:
+        print(x)
+        return str(x-24) + date_str[2:]
+    else:
+        return date_str
+
+
+# %% Convert the time format
+full_trips['arrival_time'] = full_trips['arrival_time'].apply(validate_time)
+full_trips['arrival_time'] = full_trips['arrival_time'].apply(
+    lambda x: x.replace(' ', '0'))
+full_trips['departure_time'] = full_trips['departure_time'].apply(
+    validate_time)
+full_trips['departure_time'] = full_trips['departure_time'].apply(
+    lambda x: x.replace(' ', '0'))
 
 # %%
-# stations[['latitude','longitude']] = pd.DataFrame(stations['Coords'].tolist(),index=stations.index)
-# stations.head()
-
-# %% 
-stations = stations.drop(['Coords'], axis=1)
-stations.head()
-
-# %%
-stops.to_csv('./data/stops.csv',index=False)
+full_trips['arrival_time'] = pd.to_datetime(
+    full_trips['arrival_time'], format="%H:%M:%S").dt.time
+full_trips['departure_time'] = pd.to_datetime(
+    full_trips['departure_time'], format="%H:%M:%S").dt.time
+full_trips.to_csv('./full_trips.csv', index=False)
