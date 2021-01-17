e834b097eb541249acb58c211215a0cd92b9ab1b
spiderPan <spiderPan@users.noreply.github.com>
2021-01-17 17:38:39 +0000

    deploy: 0385b6121d6dd6d48fc552c8ae9238c1d24608a6

---
+++ precache-manifest.caa7dd84c4ba45516903ef48b3acb6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd8a3c97e5833c7c4df60aead88ccee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc50ff635f537acc9ec",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "4938cd60ef10092c2e86",
+    "url": "/static/js/2.53959225.chunk.js"
+  },
+  {
+    "revision": "1b66c11fe922f2280b965988c6a12a76",
+    "url": "/static/js/2.53959225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc50ff635f537acc9ec",
+    "url": "/static/js/main.f0f426f4.chunk.js"
+  },
+  {
+    "revision": "75be811bf96334557dcb",
+    "url": "/static/js/runtime-main.c0bea3ca.js"
+  }
+]);
